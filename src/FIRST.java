743f8287fcce65244bdff38b786e485887eb0462
Sofiya18 <56328619+Sofiya18@users.noreply.github.com>
2019-11-10 15:13:52 -0600

    Have a nice day

--- src/FIRST.java
+++ src/FIRST.java
@@ -1,5 +1,8 @@
 public class FIRST {
     public static void main(String[] args) {
         System.out.println("Hi Elif");
+        System.out.println("Have a nice day");
+        System.out.println("HI");
+        System.out.println("bye");
     }
 }
